401181ac3a0cf839b1c46c32dfe90883ef0cab5b
LBBassani <lorenabassani12@gmail.com>
2019-10-01 11:59:08 -0300

    Quase contando corretamente
    A execução está durando o tempo correto, mas o controle está errado.

--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -8,14 +8,47 @@
         Necessita de Estado Inicial : Sim
 """
 import IProblema
+import threading
+from time import time
 import random
 
-def simulatedAnnealing(problema, estado, t, a, minT, numIter):
+def simulatedAnnealing(problema, estado, t, a, minT, numIter, tempo = 0):
+
+    # Rotina de tempo limite
+    def saida(encerrou):
+        if tempo > 0:
+            encerrou.clear()
+            encerrou.append([True])
+
+    encerrou = list()
+    encerrou.append([False])
+    inicio = time()
+    print(inicio)
+
+    t = threading.Timer(tempo*60, saida, encerrou)
+    t.start()
+
     melhorEstado = estado.copy()
     aux = estado.copy()
-    while t > minT:
-        viz = problema.gerarVizinhos(aux, todaVizinhanca = True)
-        for _ in range(1, numIter):
+
+    # while t > minT:
+    while True:
+        if encerrou[0][0]:
+            print("encerrou")
+            print(encerrou)
+            break
+        _ = problema.gerarVizinhos(aux, todaVizinhanca = True)
+
+    estado.clear()
+    estado.extend(melhorEstado)
+    final = time()
+    print(final)
+    t.join()
+    print("retornando")
+    return final - inicio
+
+"""        viz = problema.gerarVizinhos(aux, todaVizinhanca = True)
+         for _ in range(1, numIter):
             if len(viz) == 0:
                 break
             vizinho = viz.pop(random.randint(0, len(viz)-1))
@@ -24,6 +57,4 @@ def simulatedAnnealing(problema, estado, t, a, minT, numIter):
                 viz = problema.gerarVizinhos(aux, todaVizinhanca = True)
                 if problema.melhorEstado([aux, melhorEstado]) == aux:
                     melhorEstado = aux
-        t = t*a
-    estado.clear()
-    estado.extend(melhorEstado)
+        t = t*a """
--- trabalhoIA.py
+++ trabalhoIA.py
@@ -3,6 +3,8 @@ para o primeiro trabalho prático de IA
 """
 import IProblema
 import threading
+from simulatedAnnealing import simulatedAnnealing
+from mochila import mochila
 
 class TimedOutExc(Exception):
     pass
@@ -17,21 +19,35 @@ class treinamento:
     
     
     def treino(self, tempo = 0):
-        melhores = self.parametros[0]
-        def exiting():
-            return melhores
+        def exiting(retorno):
+            retorno.clear()
+            retorno.append([True])
 
-        t = threading.Timer(tempo*60, exiting)
+        retorno = list()
+        retorno.append([False])
+        t = threading.Timer(tempo*60, exiting, retorno)
         t.start()
         while True:
-            pass
+            if retorno[0]:
+                break
+        print("Terminou")
         
 
 class teste:
 
-    def __init__(self, problemas, metodo):
+    def __init__(self, problemas, metodo, **keyargs):
         self.problemas = problemas
         self.metodo = metodo
+        self.parametros = keyargs
 
-    def realizaTeste(self, tempo = 0):
-        pass
+    def realizaTeste(self, estado, tempo = 0):
+        return self.metodo(self.problemas[0], estado, **self.parametros, tempo = tempo)
+
+m = mochila([(1, 3), (4, 6), (5, 7)], 19)
+parametros = {"t" : 50, "a" : 0.7, "minT" : 1, "numIter" : 50}
+t = teste([m], simulatedAnnealing, **parametros)
+estado = m.estadoAleatorio()
+resposta = t.realizaTeste(estado, tempo = 0.1)
+print("Estado: ", estado)
+print("Tempo")
+print(resposta)
