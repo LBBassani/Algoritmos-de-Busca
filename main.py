93b6542f801a14894138604599a81bd3ae0d725a
LBBassani <lorenabassani12@gmail.com>
2019-10-23 21:42:31 -0300

    Rascunho para Realizar Testes

--- main.py
+++ main.py
@@ -125,10 +125,13 @@ resultadosTreinamentos["Beam Search"] = beamSearchReader.read()
 geneticoReader = resultadosFileReader("resultadoTreinamentoAlgoritmo Genetico.result")
 resultadosTreinamentos["Algoritmo Genetico"] = geneticoReader.read()
 
-print(resultadosTreinamentos)
+print("Parametros Algoritmo Genetico:", resultadosTreinamentos["Algoritmo Genetico"][0][1])
+print("Parametros Beam Search:", resultadosTreinamentos["Beam Search"][0][1])
+print("Parametros Simulated Annealing:", resultadosTreinamentos["Simulated Annealing"][0][1])
+print("Parametros GRASP:", resultadosTreinamentos["GRASP"][0][1])
 
 # problemas de Teste de acordo com apendice A do enunciado do primeiro trabalho de IA
-""" problemasTeste = {
+problemasTeste = {
     "m2"  : mochila([(1, 3), (4, 6), (5, 7)], 192), 
     "m5"  : mochila([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9), (9, 10)], 287), 
     "m7"  : mochila([(1, 2), (2, 3), (4, 5), (5, 10), (14, 15), (13, 20), (24, 25), (29, 30), (50, 50)], 120), 
@@ -139,12 +142,10 @@ print(resultadosTreinamentos)
     "m16" : mochila([(1, 3), (4, 6), (5, 7), (3, 4), (2, 6), (2, 3), (6, 8), (1, 2), (3, 5), (7, 10), (10, 15), (13, 20), (24, 25), (29, 37)], 13890), 
     "m18" : mochila([(1, 3), (4, 6), (5, 7)], 190000), 
     "m19" : mochila([(1, 3), (4, 6), (5, 7), (3, 4), (2, 6), (1, 2), (3, 5), (7, 10), (10, 15), (13, 20), (15, 20)], 4567) 
-} """
-
-
+}
 
 # resultados dos testes
-""" resultadosTestes = {
+resultadosTestes = {
     "Hill Climbing" : trabalhoIA.teste(problemasTeste, hillClimbing),
     "Beam Search" : trabalhoIA.teste(problemasTeste, beamSearch, **resultadosTreinamentos["Beam Search"][0][1]),
     "Simulated Annealing" : trabalhoIA.teste(problemasTeste, simulatedAnnealing, **resultadosTreinamentos["Simulated Annealing"][0][1]),
@@ -156,4 +157,4 @@ for key, value in resultadosTestes.items():
     resultado = value.realizaTeste()
     nomeArq = "resultadoFinal" + key + ".result"
     writer = resultadosFileWriter(nomeArq)
-    writer.write(resultado) """
+    writer.write(resultado)
